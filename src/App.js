31192956bf05d2eda91b172001cd70efe089cb7e
Ankit <singh.ankit240@gmail.com>
2021-05-28 21:49:32 +0530

    empty filled error

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home";
 import List from "./components/List";
-// import Pradeep from "./components/Pradeep";
 import './components/style.css'
-// import Count from "./components/count";
 
 function App() {
   return (
     <div className="App container font">
       <Home  />
       <List />
-      {/* <Pradeep /> */}
-      {/* <Count /> */}
     </div>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-// var itemList = ['A-Apple', 'B-Ball', 'C-Cat']
 
 class List extends Component {
     constructor(props) {
@@ -18,8 +17,12 @@ class List extends Component {
     }
 
     onAddItem = () => {
+        if (!this.state.value){
+            alert("filled the task")
+        }else{
         const itemList = this.state.itemList.concat(this.state.value)
         this.setState({ itemList, value: '' })
+        }
     }
 
     onRemoveItem = i => {
@@ -33,7 +36,6 @@ class List extends Component {
         this.setState({ itemList, reset })
     }
     onResetArray() {
-        console.log(this.state.itemList);
         if (this.state.itemList.length === 0){
             const itemList = this.state.reset
             this.setState({ itemList })
@@ -43,12 +45,14 @@ class List extends Component {
     render() {
         return (
             <div>
-                {this.state.itemList.map((item, index) => (
+                {this.state.itemList.map((item, index) => {
+                    console.log(item, index)
+                    return(
                     <div>
                         <h3 key={index}>{item}</h3>
                         <button className="btn btn-light mt-2 mb-4" onClick={() => this.onRemoveItem(index)}>Remove</button>
-                    </div>
-                ))}
+                    </div>)
+                })}
                 <input type="text" value={this.state.value} onChange={this.onChangeValue} />
                 <br />
                 <button className="btn btn-warning m-2" onClick={this.onAddItem}>Add</button>
